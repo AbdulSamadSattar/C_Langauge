257672931f721bf6470c746fc310a5440358dffb
Abdul Samad <155386298+AbdulSamadSattar@users.noreply.github.com>
2026-08-16 15:10:41 +0500

    Delete VScodeHarry directory

--- VScodeHarry/reference_variable.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    float x= 455.9f;
-    float &y = x; 
-    int integer = 9;
-    cout<<int(x)<<endl<<y<<endl<<float(integer);} //typecasting
